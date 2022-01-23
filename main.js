07717266777b1e01195f93f57d2513a143ce92c3
Sourav Barui <74421679+im-sourav@users.noreply.github.com>
2022-01-24 02:25:25 +0530

    ss
    ss

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const intreWindow = document.getElementById("intre_window");
 const sSelector = document.getElementById("s_contaner");
 const selector = document.getElementById("selector");
 const corner = document.querySelectorAll(".corner");
@@ -24,10 +25,14 @@ root.style.setProperty("--height", `${minSize}px`);
 
 let selectWidth = 100;
 let inkb = 512;
+let squir = false;
 
 // set inkb value
 imgSizeInput.addEventListener("keyup", (e) => {
-  if (!imgSizeInput.value || 0 >= imgSizeInput.value) return;
+  if (!imgSizeInput.value || 0 >= imgSizeInput.value) {
+    inkb = 512;
+    return;
+  }
   inkb = Number(imgSizeInput.value);
 });
 
@@ -72,7 +77,6 @@ let sids = {
 
 const cnr = [selector, corner[0], corner[1], corner[2], corner[3]];
 let isCorner = false;
-let s = selector.style;
 
 cnr.forEach((elmt, i) => {
   let lx, ly, dx, dy;
@@ -92,24 +96,44 @@ cnr.forEach((elmt, i) => {
       isCorner = true;
     }
     if (elmt === corner[0] && sids.l + dx > 0 && sids.t + dy > 0) {
-      let min = Math.floor((dx + dy) / 2);
-      sids.l += min;
-      sids.t += min;
+      if (squir) {
+        let min = Math.floor((dx + dy) / 2);
+        sids.l += min;
+        sids.t += min;
+      } else {
+        sids.l += dx;
+        sids.t += dy;
+      }
       cssSides();
     } else if (elmt === corner[1] && sids.r - dx > 0 && sids.t + dy > 0) {
-      let min = Math.floor((-dx + dy) / 2);
-      sids.r += min;
-      sids.t += min;
+      if (squir) {
+        let min = Math.floor((-dx + dy) / 2);
+        sids.r += min;
+        sids.t += min;
+      } else {
+        sids.r += -dx;
+        sids.t += dy;
+      }
       cssSides();
     } else if (elmt === corner[2] && sids.r - dx > 0 && sids.b - dy > 0) {
-      let min = Math.floor((-dx + -dy) / 2);
-      sids.r += min;
-      sids.b += min;
+      if (squir) {
+        let min = Math.floor((-dx + -dy) / 2);
+        sids.r += min;
+        sids.b += min;
+      } else {
+        sids.r += -dx;
+        sids.b += -dy;
+      }
       cssSides();
     } else if (elmt === corner[3] && sids.l + dx > 0 && sids.b - dy > 0) {
-      let min = Math.floor((dx + -dy) / 2);
-      sids.l += min;
-      sids.b += min;
+      if (squir) {
+        let min = Math.floor((dx + -dy) / 2);
+        sids.l += min;
+        sids.b += min;
+      } else {
+        sids.l += dx;
+        sids.b += -dy;
+      }
       cssSides();
     } else if (
       !isCorner &&
@@ -133,30 +157,30 @@ cnr.forEach((elmt, i) => {
     }
   });
 });
+const img = new Image();
+let ratio = 0,
+  nsize = 0;
 
 imgInput.addEventListener("change", (e) => {
+  if (!e.target.files[0]) return;
+  sSelector.style.display = "flex";
+  fullScreen();
   sids = {
     r: 25,
     l: 25,
     t: 25,
     b: 25,
   };
+  ratio = 0;
+  nsize = 0;
   cssSides();
-  if (!e.target.files[0]) return;
   let Img = URL.createObjectURL(e.target.files[0]);
-  const img = new Image();
   img.src = Img;
   img.onload = () => {
     let scale = selectWidth / img.width;
-
-    imgResize.style.backgroundSize = `${minSize}px ${minSize}px`;
-
     let w = img.width,
       h = img.height,
-      ss = sSelector.style,
-      ns = selector.style,
-      ratio,
-      nsize;
+      ss = sSelector.style;
 
     if (h > w) {
       ratio = h / minSize;
@@ -165,10 +189,12 @@ imgInput.addEventListener("change", (e) => {
       ss.width = `${nsize}px`;
       ss.height = `${minSize}px`;
       let margin = Math.floor(minSize - nsize) / 2;
-      sids.t = 25 + margin;
-      sids.b = 25 + margin;
-      ns.top = `${25 + margin}px`;
-      ns.bottom = `${25 + margin}px`;
+      if (squir) {
+        sids.t = 25 + margin;
+        sids.b = 25 + margin;
+        root.style.setProperty("--st", `${25 + margin}px`);
+        root.style.setProperty("--sb", `${25 + margin}px`);
+      }
     } else {
       ratio = w / minSize;
       nsize = h / ratio;
@@ -176,67 +202,89 @@ imgInput.addEventListener("change", (e) => {
       ss.width = `${minSize}px`;
       ss.height = `${nsize}px`;
       let margin = Math.floor(minSize - nsize) / 2;
-      sids.l = 25 + margin;
-      sids.r = 25 + margin;
-      ns.left = `${25 + margin}px`;
-      ns.right = `${25 + margin}px`;
+      if (squir) {
+        sids.l = 25 + margin;
+        sids.r = 25 + margin;
+        root.style.setProperty("--sl", `${25 + margin}px`);
+        root.style.setProperty("--sr", `${25 + margin}px`);
+      }
     }
 
     ss.backgroundImage = `url('${Img}')`;
-
-    crapBtn.addEventListener("click", () => {
-      preview.style.display = "flex";
-
-      let left = selector.offsetLeft,
-        top = selector.offsetTop,
-        wid = selector.clientWidth,
-        hei = selector.clientHeight;
-
-      function maekImgPerfectSize() {
-        $$cvs.width = selectWidth;
-        $$cvs.height = selectWidth;
-        $$ctx.clearRect(0, 0, $$cvs.width, $$cvs.height);
-        $$ctx.drawImage(
-          img,
-          left * ratio,
-          top * ratio,
-          wid * ratio,
-          hei * ratio,
-          0,
-          0,
-          selectWidth,
-          selectWidth
-        );
-        return (newImgSrc = $$ctx.canvas.toDataURL(e.target, "image/jpeg"));
-      }
-
-      while (maekImgPerfectSize().length / 1000 < inkb * 1.35) {
-        selectWidth += 100;
-      }
-      while (maekImgPerfectSize().length / 1000 > inkb * 1.35) {
-        selectWidth -= 10;
-      }
-      console.log(maekImgPerfectSize().length / 1000);
-      imgResize.style.backgroundImage = `url(${maekImgPerfectSize()})`;
-
-      downloadBtn.addEventListener("click", () => {
-        if (window.navigator.msSaveBlob) {
-          window.navigator.msSaveBlob($$cvs.msToBlob(), "your-image.png");
-        } else {
-          const a = document.createElement("a");
-          document.body.appendChild(a);
-          a.href = $$cvs.toDataURL();
-          a.download = "your-image.png";
-          a.click();
-          document.body.removeChild(a);
-        }
-      });
-    });
   };
 });
 
+crapBtn.addEventListener("click", () => {
+  preview.style.display = "flex";
+
+  let left = selector.offsetLeft,
+    top = selector.offsetTop,
+    wid = selector.clientWidth,
+    hei = selector.clientHeight,
+    scaleN = wid / selectWidth,
+    cw,
+    ch;
+
+  function maekImgPerfectSize() {
+    if (squir) {
+      $$cvs.width = selectWidth;
+      $$cvs.height = selectWidth;
+      cw = selectWidth;
+      ch = selectWidth;
+    } else {
+      $$cvs.width = selectWidth;
+      scaleN = selector.clientWidth / selectWidth;
+      $$cvs.height = hei / scaleN;
+      cw = selectWidth;
+      ch = hei / scaleN;
+    }
+    $$ctx.clearRect(0, 0, $$cvs.width, $$cvs.height);
+    $$ctx.drawImage(
+      img,
+      left * ratio,
+      top * ratio,
+      wid * ratio,
+      hei * ratio,
+      0,
+      0,
+      $$cvs.width,
+      $$cvs.height
+    );
+    return (newImgSrc = $$ctx.canvas.toDataURL(img, "image/jpeg"));
+  }
+  while (maekImgPerfectSize().length / 1000 < inkb * 1.35) {
+    selectWidth += 100;
+  }
+  while (maekImgPerfectSize().length / 1000 > inkb * 1.35) {
+    selectWidth -= 10;
+  }
+
+  if (squir) {
+    imgResize.style.backgroundSize = `${minSize}px ${minSize}px`;
+  } else {
+    let scl = ch / cw;
+    imgResize.style.backgroundSize = `${minSize}px ${minSize * scl}px`;
+    root.style.setProperty("--width", `${minSize}px`);
+    root.style.setProperty("--height", `${minSize * scl}px`);
+  }
+
+  imgResize.style.backgroundImage = `url(${maekImgPerfectSize()})`;
+
+  downloadBtn.addEventListener("click", () => {
+    if (window.navigator.msSaveBlob) {
+      window.navigator.msSaveBlob($$cvs.msToBlob(), "your-image.png");
+    } else {
+      const a = document.createElement("a");
+      document.body.appendChild(a);
+      a.href = $$cvs.toDataURL();
+      a.download = "your-image.png";
+      a.click();
+      document.body.removeChild(a);
+    }
+  });
+});
+
 // canvas propatys -----------
-// preview.appendChild($$cvs);
 
 pvuBtn.addEventListener("click", () => {
   preview.style.display = "flex";
@@ -255,9 +303,18 @@ function getFullScreen() {
   );
 }
 
-document.body.addEventListener("click", () => {
-  if(getFullScreen())return;
-  if (!getFullScreen()) {
-    document.documentElement.requestFullscreen();
-  }
+document.body.addEventListener("click", () => {});
+
+intreWindow.addEventListener("click", () => {
+  fullScreen();
+  intreWindow.style.display = "none";
 });
+
+function fullScreen() {
+  if (getFullScreen()) return;
+  if (!getFullScreen()) {
+    document.documentElement
+      .requestFullscreen()
+      .catch(console.log());
+  }
+}
